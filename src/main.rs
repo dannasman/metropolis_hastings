bb4ceb423ba8455f593d3560fb04e90af46a766e
Dan Nasman <dan.nasman@gmail.com>
2023-04-10 20:15:14 +0300

    add hilbert curve

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,80 @@
 use std::env;
 use std::error::Error;
+use std::time::Instant;
 
 mod metropolis_hastings;
 use metropolis_hastings::SpinGrid;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let filename = env::args()
-        .nth(1)
-        .expect("second argument must be file name");
-    let dim_x = std::env::args()
-        .nth(2)
-        .expect("third argument must be length of the grid along x-axis")
-        .parse::<usize>()?;
-    let dim_y = std::env::args()
-        .nth(3)
-        .expect("fourth argument must be length of the grid along y-axis")
-        .parse::<usize>()?;
-    let inter_strength = std::env::args()
-        .nth(4)
-        .expect("fifth argument must be interaction strength J/(kT)")
-        .parse::<f64>()?;
-    let temperature = std::env::args()
-        .nth(5)
-        .expect("sixth argument must be temperature T in Kelvin")
-        .parse::<f64>()?;
+    if env::args().len() == 6 {
+        let filename = env::args()
+            .nth(1)
+            .expect("second argument must be file name");
+        let order = std::env::args()
+            .nth(2)
+            .expect("third argument must be the order of the Hilbert curve")
+            .parse::<usize>()?;
+        let inter_strength = std::env::args()
+            .nth(3)
+            .expect("fourth argument must be interaction strength J/(kT)")
+            .parse::<f64>()?;
+        let temperature = std::env::args()
+            .nth(4)
+            .expect("eight argument must be temperature T in Kelvin")
+            .parse::<f64>()?;
 
-    let iterations = std::env::args()
-        .nth(6)
-        .expect("seventh argument must be number of Monte Carlo simulation steps")
-        .parse::<u32>()?;
+        let iterations = std::env::args()
+            .nth(5)
+            .expect("sixth argument must be number of Monte Carlo simulation steps")
+            .parse::<u32>()?;
 
-    let mut spin_grid = SpinGrid::new(dim_x, dim_y);
-    let mut i = 0;
-    while i < iterations {
-        spin_grid.calculate_configurations(inter_strength, temperature);
-        i += 1;
+        let mut spin_grid = SpinGrid::new_hcurve(order);
+        let mut i = 0;
+        let now = Instant::now();
+        while i < iterations {
+            spin_grid.calculate_configurations_hcurve(inter_strength, temperature)?;
+            i += 1;
+        }
+        let elapsed = now.elapsed();
+        println!("Simulation completed in {:?}", elapsed);
+        spin_grid.save(&filename)?;
     }
-    spin_grid.save(&filename)?;
+    else if env::args().len() == 7 {
+        let filename = env::args()
+            .nth(1)
+            .expect("second argument must be file name");
+        let dim_x = std::env::args()
+            .nth(2)
+            .expect("third argument must be length of the grid along x-axis")
+            .parse::<usize>()?;
+        let dim_y = std::env::args()
+            .nth(3)
+            .expect("fourth argument must be length of the grid along y-axis")
+            .parse::<usize>()?;
+        let inter_strength = std::env::args()
+            .nth(4)
+            .expect("fifth argument must be interaction strength J/(kT)")
+            .parse::<f64>()?;
+        let temperature = std::env::args()
+            .nth(5)
+            .expect("sixth argument must be temperature T in Kelvin")
+            .parse::<f64>()?;
+
+        let iterations = std::env::args()
+            .nth(6)
+            .expect("seventh argument must be number of Monte Carlo simulation steps")
+            .parse::<u32>()?;
+
+        let mut spin_grid = SpinGrid::new(dim_x, dim_y);
+        let mut i = 0;
+        let now = Instant::now();
+        while i < iterations {
+            spin_grid.calculate_configurations(inter_strength, temperature);
+            i += 1;
+        }
+        let elapsed = now.elapsed();
+        println!("Simulation completed in {:?}", elapsed);
+        spin_grid.save(&filename)?;
+    }
     Ok(())
 }
--- src/metropolis_hastings/grid.rs
+++ src/metropolis_hastings/grid.rs
@@ -6,6 +6,7 @@ use std::io::{Error, Write};
 pub struct SpinGrid {
     dim_x: usize,
     dim_y: usize,
+    order: Option<usize>,
     pub grid: Vec<Vec<i32>>, //2D spin grid
 }
 
@@ -14,14 +15,25 @@ impl SpinGrid {
         SpinGrid {
             dim_x,
             dim_y,
+            order: None,
             grid: vec![vec![1; dim_y]; dim_x], //initialize grid with all spins up
+        }
+    }
+
+    pub fn new_hcurve(order: usize) -> Self {
+        let dim = 1 << order;
+        SpinGrid {
+            dim_x: dim,
+            dim_y: dim,
+            order: Some(order),
+            grid: vec![vec![1; dim]; dim],
         }
     }
 
     pub fn calculate_configurations(&mut self, inter_strength: f64, temperature: f64) {
         let mut rng = rand::thread_rng();
 
-        let n = (self.dim_x) * (self.dim_y);
+        let n = self.dim_x * self.dim_y;
         for _i in 0..n {
             let x: usize = rng.gen_range(0..self.dim_x);
             let y: usize = rng.gen_range(0..self.dim_y);
@@ -51,6 +63,80 @@ impl SpinGrid {
             if energy < 0.0 || rn < (-energy / temperature).exp() {
                 self.grid[x][y] = -self.grid[x][y];
             }
+        }
+    }
+
+    pub fn calculate_configurations_hcurve(
+        &mut self,
+        inter_strength: f64,
+        temperature: f64,
+    ) -> Result<(), &'static str> {
+        let mut rng = rand::thread_rng();
+
+        //parallel prefix method is used to get (x, y) from Hilbert curve distance s
+        if let Some(n) = self.order {
+            let max_length = (1 << n) * (1 << n);
+            for i in 0..max_length {
+                let mut s = i | (0x55555555 << 2 * n);
+                let sr = (s >> 1) & 0x55555555;
+                let mut cs = ((s & 0x55555555) + sr) ^ 0x55555555;
+
+                cs = cs ^ (cs >> 2);
+                cs = cs ^ (cs >> 4);
+                cs = cs ^ (cs >> 8);
+                cs = cs ^ (cs >> 16);
+                let swap = cs & 0x55555555;
+                let comp = (cs >> 1) & 0x55555555;
+
+                let mut t = (s & swap) ^ comp;
+                s = s ^ sr ^ t ^ (t << 1);
+
+                s = s & ((1 << 2 * n) - 1);
+
+                t = (s ^ (s >> 1)) & 0x22222222;
+                s = s ^ t ^ (t << 1);
+
+                t = (s ^ (s >> 2)) & 0x0C0C0C0C;
+                s = s ^ t ^ (t << 2);
+
+                t = (s ^ (s >> 4)) & 0x00F000F0;
+                s = s ^ t ^ (t << 4);
+
+                t = (s ^ (s >> 8)) & 0x0000FF00;
+                s = s ^ t ^ (t << 8);
+
+                let x = s >> 16;
+                let y = s & 0xFFFF;
+                let sigma_xy = self.grid[x][y] as f64;
+                let mut sum = 0;
+
+                if x < self.dim_x - 1 {
+                    sum += self.grid[x + 1][y];
+                }
+
+                if x > 0 {
+                    sum += self.grid[x - 1][y];
+                }
+
+                if y < self.dim_y - 1 {
+                    sum += self.grid[x][y + 1];
+                }
+
+                if y > 0 {
+                    sum += self.grid[x][y - 1];
+                }
+
+                let s = sum as f64;
+                let energy = 2.0 * inter_strength * sigma_xy * s;
+
+                let rn: f64 = rng.gen();
+                if energy < 0.0 || rn < (-energy / temperature).exp() {
+                    self.grid[x][y] = -self.grid[x][y];
+                }
+            }
+            Ok(())
+        } else {
+            Err("to calculate configuration with Hilbert curve use SpinGrid::new_hcurve(order) when creating the grid")
         }
     }
 
